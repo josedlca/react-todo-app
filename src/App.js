cf07486278cebcbcebf92c8705ada6a5954f2c37
josedlca <jose.dlcruz.ayala@gmail.com>
2021-02-04 12:17:22 -0500

    Todo list react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <input type="checkbox"/>
+          <p>One</p>
+        </li>
+        <li>
+          <input type="checkbox"/>
+          <p>two</p>
+        </li>
+        <li>
+          <input type="checkbox"/>
+          <p>Three</p>
+        </li>
+        <li>
+          <input type="checkbox"/>
+          <p>Four</p>
+        </li>
+      </ul>
     </div>
   );
 }
