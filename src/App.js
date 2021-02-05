f2a7610e14d870a4aea4689ee23f12d60b26d697
josedlca <jose.dlcruz.ayala@gmail.com>
2021-02-05 17:05:16 -0500

    onChange function added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React ,{Component}from 'react'
 import './App.css';
 import TodoItem from './TodoItem'
 import todosData from './todosData'
 
-function App() {
-  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      allData: todosData
+    }
 
-  return (
-    <div className="App todo-list">
-      {todoItems}
-    </div>
-  );
+    this.handleChange = this.handleChange.bind(this)
+
+  }
+
+  handleChange(id){
+    this.setState(prevState => {
+      const updatedTodos = prevState.allData.map(todo => {
+        if(todo.id === id){
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return{
+        allData: updatedTodos
+      }
+    })
+  }
+
+  render(){
+    const todoItems = this.state.allData.map(item => <TodoItem key={item.id} item={item} handleChange= {this.handleChange}/>)
+    
+    return(
+      <div className="App todo-list">
+        {todoItems}
+      </div>
+    )
+  }
 }
 
 export default App;
